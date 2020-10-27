ec169482d5a4cfa0f65bb30c161b1aabd9724faf
zaid-nawaz <zaidnawaz0005@gmail.com>
2020-10-27 05:45:58 -0700

    nothing

--- bid/seesomeproduct/urls.py
+++ bid/seesomeproduct/urls.py
@@ -4,6 +4,6 @@ from seesomeproduct import views
 
 urlpatterns = [
     url(r'^$',views.seesomeproduct,name='seesomeproduct'),
-    url(r'^searchresults/',views.search,name='search')
+    url(r'^searchresults/',views.search,name='search'),
 
 ] 
